a8ce19552f173a8c493b47d293e7bb70482f948c
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-05-21 19:38:12 -0400

    Update certificate_edit.php

--- admin/certificate_edit.php
+++ admin/certificate_edit.php
@@ -10,12 +10,12 @@ use PhpOffice\PhpWord\Style\Paragraph;
 
 if (isset($_GET['empid'])) {
 
-    $empid = $_GET['empid'];
+  $empid = $_GET['empid'];
 
-    include 'includes/session.php';
+  include 'includes/session.php';
 
-    // Obtener los datos del empleado utilizando el empid
-    $sql = "SELECT *, employees.id AS empid
+  // Obtener los datos del empleado utilizando el empid
+  $sql = "SELECT *, employees.id AS empid
     FROM employees
     LEFT JOIN position ON position.id = employees.position_id
     LEFT JOIN schedules ON schedules.id = employees.schedule_id
@@ -31,82 +31,90 @@ if (isset($_GET['empid'])) {
     ) AS attendance_hours ON employees.id = attendance_hours.employee_id
     WHERE employees.employee_id = '$empid'";
 
-    $query = $conn->query($sql);
-    $employee = $query->fetch_assoc();
+  $query = $conn->query($sql);
+  $employee = $query->fetch_assoc();
 
-    // Verificar si se obtuvieron datos del empleado antes de acceder a sus propiedades.
-    if (!empty($employee) && isset($employee['firstname'], $employee['lastname'], $employee['identity_card'], $employee['type_modality'], $employee['description'], $employee['name_area'], $employee['totalHours'], $employee['created_on'])) {
-        
-        // Crear una instancia de PHPWord
-        $phpWord = new PhpWord();
+  // Verificar si se obtuvieron datos del empleado antes de acceder a sus propiedades.
+  if (!empty($employee) && isset($employee['firstname'], $employee['lastname'], $employee['identity_card'], $employee['type_modality'], $employee['description'], $employee['name_area'], $employee['totalHours'], $employee['created_on'])) {
 
-        // Agregar una nueva sección al documento
-        $section = $phpWord->addSection();
+    // Crear una instancia de PHPWord
+    $phpWord = new PhpWord();
 
-        // Obtener la fecha actual en español
-        $meses = array(
-            'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
-            'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
-        );
-        $fecha_actual = date('d') . ' de ' . $meses[date('n') - 1] . ' del ' . date('Y');
-        $fecha_creacion = date('d', strtotime($employee['created_on'])) . ' de ' . $meses[date('n', strtotime($employee['created_on'])) - 1] . ' del ' . date('Y', strtotime($employee['created_on']));
+    // Configuración de la página en formato carta
+    $section = $phpWord->addSection(array(
+      'pageSizeW' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(8.5),
+      'pageSizeH' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(11),
+      'marginTop' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(1),
+      'marginLeft' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(1),
+      'marginRight' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(1),
+      'marginBottom' => \PhpOffice\PhpWord\Shared\Converter::inchToTwip(1)
+    ));
 
-        // Crear un TextRun centrado para los títulos
-        $titleStyle = array('bold' => true, 'size' => 15, 'underline' => 'single');
-        $subtitleStyle = array('bold' => true, 'size' => 13, 'underline' => 'single');
+    // Obtener la fecha actual en español
+    $meses = array(
+      'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio',
+      'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'
+    );
+    $fecha_actual = date('d') . ' de ' . $meses[date('n') - 1] . ' del ' . date('Y');
+    $fecha_creacion = date('d', strtotime($employee['created_on'])) . ' de ' . $meses[date('n', strtotime($employee['created_on'])) - 1] . ' del ' . date('Y', strtotime($employee['created_on']));
 
-        $title = $section->addTextRun(array('alignment' => Jc::CENTER));
-        $title->addText('CERTIFICADO DE PRÁCTICA PROFESIONAL', $titleStyle);
-        $section->addTextBreak(1);
+    // Crear un TextRun centrado para los títulos
+    $titleStyle = array('bold' => true, 'size' => 15, 'underline' => 'single');
+    $subtitleStyle = array('bold' => true, 'size' => 13, 'underline' => 'single');
+    $section->addTextBreak(2);
+    $title = $section->addTextRun(array('alignment' => Jc::CENTER));
+    $title->addText('CERTIFICADO DE PRÁCTICA PROFESIONAL', $titleStyle);
+    $section->addTextBreak(1);
 
-        $subtitle = $section->addTextRun(array('alignment' => Jc::CENTER));
-        $subtitle->addText('AGBC/DAF/RRHH-CPP-N°0/' . date('Y'), $subtitleStyle);
-        $section->addTextBreak(2);
+    $subtitle = $section->addTextRun(array('alignment' => Jc::CENTER));
+    $subtitle->addText('AGBC/DAF/RRHH-CPP-N°0/' . date('Y'), $subtitleStyle);
+    $section->addTextBreak(2);
 
-        // Configuración de alineación justificada
-        $paragraphStyle = array('align' => 'both', 'spaceAfter' => 240);
+    // Configuración de alineación justificada
+    $paragraphStyle = array('alignment' => Jc::BOTH, 'spaceAfter' => 240);
 
-        $section->addText('El suscrito Lic. Boris Mario Miranda Velasco, Director Administrativo Financiero de la Agencia Boliviana de Correos - AGBC, en uso de las atribuciones conferidas por Ley.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
-        $section->addTextBreak(1);
-        $section->addTitle('CERTIFICA A QUIEN CORRESPONDA:', 2);
-        $section->addText('Que el Sr./Sra.: ' . $employee['firstname'] . ' ' . $employee['lastname'] . ', con Cédula de Identidad N° ' . $employee['identity_card'] . ' expedido en la ciudad de La Paz, realizó su ' . $employee['type_modality'] . ' en la ' . $employee['description'] . ' - ' . $employee['name_area'] . ' de nuestra institución, cumpliendo una carga horaria acumulada de ' . $employee['totalHours'] . ' horas de trabajo, con fecha de inicio desde el ' . $fecha_creacion . ' hasta el ' . $fecha_actual . ' .', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
-        $section->addTextBreak(1);
-        $section->addText('Durante su permanencia en la Agencia, el Sr./Sra.: ' . $employee['firstname'] . ' ' . $employee['lastname'] . ' ha demostrado excelente aptitud, responsabilidad, puntualidad y colaboración en el desempeño de las funciones asignadas, desarrollando un alto grado de compromiso con la Agencia Boliviana de Correos - AGBC.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
-        $section->addTextBreak(1);
-        $section->addText('Exhortamos al estudiante a continuar con ese mismo espíritu, para bien personal y su respectiva proyección profesional, augurándole por nuestra parte permanente éxito.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
-        $section->addTextBreak(1);
-        $section->addText('Es cuanto tengo a bien certificar en honor a la verdad, para fines consiguientes.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
-        $section->addTextBreak(1);
-        $section->addText('La Paz, Fecha: ' . $fecha_actual, ['name' => 'Arial', 'size' => 12, 'align' => 'right']);
+    $section->addText('El suscrito Lic. Boris Mario Miranda Velasco, Director Administrativo Financiero de la Agencia Boliviana de Correos - AGBC, en uso de las atribuciones conferidas por Ley.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
+    $section->addTextBreak(1);
+    $section->addText('CERTIFICA A QUIEN CORRESPONDA:', ['name' => 'Arial', 'size' => 13, 'bold' => true], $paragraphStyle);
+    $section->addTextBreak(1);
+    $section->addText('Que el Sr./Sra.: ' . $employee['firstname'] . ' ' . $employee['lastname'] . ', con Cédula de Identidad N° ' . $employee['identity_card'] . ' expedido en la ciudad de La Paz, realizó su ' . $employee['type_modality'] . ' en la ' . $employee['description'] . ' - ' . $employee['name_area'] . ' de nuestra institución, cumpliendo una carga horaria acumulada de ' . $employee['totalHours'] . ' horas de trabajo, con fecha de inicio desde el ' . $fecha_creacion . ' hasta el ' . $fecha_actual . ' .', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
+    $section->addTextBreak(1);
+    $section->addText('Durante su permanencia en la Agencia, el Sr./Sra.: ' . $employee['firstname'] . ' ' . $employee['lastname'] . ' ha demostrado excelente aptitud, responsabilidad, puntualidad y colaboración en el desempeño de las funciones asignadas, desarrollando un alto grado de compromiso con la Agencia Boliviana de Correos - AGBC.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
+    $section->addTextBreak(1);
+    $section->addText('Exhortamos al estudiante a continuar con ese mismo espíritu, para bien personal y su respectiva proyección profesional, augurándole por nuestra parte permanente éxito.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
+    $section->addTextBreak(1);
+    $section->addText('Es cuanto tengo a bien certificar en honor a la verdad, para fines consiguientes.', ['name' => 'Arial', 'size' => 12], $paragraphStyle);
+    $section->addTextBreak(1);
+    // Texto alineado a la derecha
+    $section->addText('La Paz, Fecha: ' . $fecha_actual, ['name' => 'Arial', 'size' => 12, 'alignment' => Jc::RIGHT]);
 
-        // Guardar el documento en un archivo temporal
-        $tempFile = tempnam(sys_get_temp_dir(), 'PHPWord');
-        $writer = IOFactory::createWriter($phpWord, 'Word2007');
-        $writer->save($tempFile);
 
-        // Forzar la descarga del archivo DOCX
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-        header('Content-Disposition: attachment; filename="certificado_practica.docx"');
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($tempFile));
-        ob_clean();
-        flush();
-        readfile($tempFile);
-        unlink($tempFile);
-        exit();
-    } else {
-        // Si faltan datos o el array está vacío, redirigir a otra página o mostrar un mensaje de error
-        header('Location: error_page.php');
-        exit();
-    }
+    // Guardar el documento en un archivo temporal
+    $tempFile = tempnam(sys_get_temp_dir(), 'PHPWord');
+    $writer = IOFactory::createWriter($phpWord, 'Word2007');
+    $writer->save($tempFile);
 
-} else {
-    // Redirigir si no se proporciona el parámetro empid
-    header('Location: index.php');
+    // Forzar la descarga del archivo DOCX
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    header('Content-Disposition: attachment; filename="certificado_practica.docx"');
+    header('Content-Transfer-Encoding: binary');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($tempFile));
+    ob_clean();
+    flush();
+    readfile($tempFile);
+    unlink($tempFile);
     exit();
+  } else {
+    // Si faltan datos o el array está vacío, redirigir a otra página o mostrar un mensaje de error
+    header('Location: error_page.php');
+    exit();
+  }
+} else {
+  // Redirigir si no se proporciona el parámetro empid
+  header('Location: index.php');
+  exit();
 }
-?>
