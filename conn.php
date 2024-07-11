0ef0daf05b84d2e594b79495524708b7786b826e
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-11 15:17:08 -0400

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('172.65.10.170', 'agbc', 'agbc', 'stazher');
+	$conn = new mysqli('localhost', 'root', '', 'stazher');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
